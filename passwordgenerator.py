fabae1935c3076f6e9734a8b00faf3398f774c8e
Chaitany Chaudhary <85825826+ChaitanyChaudhary@users.noreply.github.com>
2023-06-17 21:42:08 +0530

    passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,10 +1,3 @@
-''' secrets module: This module is used for generating random numbers for managing important data such as passwords, account authentication,
-security tokens, etc, that are cryptographically strong. 
-
-string module: This module provides string constants that we can use to define the alphabet. 
-
-'''
-
 import secrets
 import string
 
@@ -26,10 +19,9 @@ print(passwd)
 
 # To repeat above process for the length of the password use loop.
 while True:
-  passwd = ''.join(secrets.choice(alphabet) for i in range(passwd_length))
+    passwd = ''.join(secrets.choice(alphabet) for i in range(passwd_length))
 
-  if (any(char in special_chars for char in passwd)
-      and sum(char in digits for char in passwd) >= 2):
-    break
+    if (any(char in special_chars for char in passwd) and sum(char in digits for char in passwd) >= 2):
+        break
 print("\nPassword 2: \n")
 print(passwd)
